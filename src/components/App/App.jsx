321f8f6fe0f632c5c7de99a5ce3614cdb6d33845
Andrii <maystrenko22a@gmail.com>
2021-11-08 23:14:17 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,8 +16,14 @@ class App extends Component {
   };
   addContacts = contact => {
     const inputName = contact.name;
+    const inputNumber = contact.number;
     this.setState(prev => {
-      if (prev.contacts.every(contact => contact.name !== inputName)) {
+      if (
+        prev.contacts.every(
+          contact =>
+            contact.name !== inputName && inputNumber !== contact.number,
+        )
+      ) {
         return {
           contacts: [contact, ...prev.contacts],
         };
@@ -32,7 +38,7 @@ class App extends Component {
 
   render() {
     const { contacts, filter } = this.state;
-    const filterNormalized = filter;
+    const filterNormalized = filter.toLowerCase();
     const filterContacts = contacts.filter(contact =>
       contact.name.toLowerCase().includes(filterNormalized),
     );
